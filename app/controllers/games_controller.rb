aa81ca6475c817a7633a7d2ac7c30732dfc4892e
YY T <changiyy07@gmail.com>
2024-02-20 22:50:01 +0800

    add root route and remove require after reading Rails guide

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,26 @@
-require 'rest-client'
-require 'json'
+# require 'rest-client' not required as alr loaded into Gemfile
+# require 'json' not required as alr loaded into Gemfile
 
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { [*'A'..'Z'].sample }
   end
+
+  def score
+    english_word = english_word?
+    included = word_included?
+    if english_word
+      if included
+        @message = "Congratulations! #{params[:word]} is a valid English word"
+      else
+        @message = "Sorry, #{params[:word]} can't be built out of #{params[:letters]}."
+      end
+    else
+      @message = "Sorry, #{params[:word]} does not seem to be a valid English word..."
+    end
+  end
+
+  private
 
   def english_word?
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
@@ -17,19 +33,5 @@ class GamesController < ApplicationController
     words_array = params[:word].upcase.chars
     letters_array = params[:letters].split
     words_array.all? { |word| words_array.count(word) <= letters_array.count(word) }
-  end
-
-  def score
-    english_word = english_word?
-    included = word_included?
-    if english_word == true
-      if included == true
-        @message = "Congratulations! #{params[:word]} is a valid English word"
-      else
-        @message = "Sorry, #{params[:word]} can't be built out of #{params[:letters]}."
-      end
-    else
-      @message = "Sorry, #{params[:word]} does not seem to be a valid English word..."
-    end
   end
 end
